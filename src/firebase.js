ff7964b11c672c02c5b235bbf93b3d8e67eea21e
shreyaskumbhar4-hub <shreyaskumbhar4@gmail.com>
2026-05-22 21:58:37 +0530

    tempurary create the secrets path for api for app creation

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 
 // Firebase config
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: secrets.VITE_FIREBASE_API_KEY,
+  authDomain: secrets.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: secrets.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: secrets.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: secrets.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: secrets.VITE_FIREBASE_APP_ID,
+  measurementId: secrets.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
